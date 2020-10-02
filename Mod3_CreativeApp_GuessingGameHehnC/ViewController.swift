4fe3cca36597e8dc167bc2a54b36c5f5b1e0564d
Colin Hehn <colin45h@yahoo.com>
2020-10-01 21:56:25 -0500

    Initial Commit

---
+++ Mod3_CreativeApp_GuessingGameHehnC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mod3_CreativeApp_GuessingGameHehnC
+//
+//  Created by Tiger Coder on 10/1/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
